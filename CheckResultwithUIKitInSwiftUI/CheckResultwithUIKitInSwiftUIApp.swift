8df688ff853efcec5d4f45757ab610e1c86f3566
Alex Rybachev <rybachev.as@gmail.com>
2022-05-23 08:32:20 +0400

    Initial Commit

---
+++ CheckResultwithUIKitInSwiftUI/CheckResultwithUIKitInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckResultwithUIKitInSwiftUIApp.swift
+//  CheckResultwithUIKitInSwiftUI
+//
+//  Created by Aleksandr Rybachev on 23.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CheckResultwithUIKitInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
