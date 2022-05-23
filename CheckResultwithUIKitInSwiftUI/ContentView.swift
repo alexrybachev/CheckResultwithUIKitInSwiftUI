7ea8ac8b9d113a3aac200a67a7101259f8587b76
Alex Rybachev <rybachev.as@gmail.com>
2022-05-23 21:44:13 +0400

    add check value

--- CheckResultwithUIKitInSwiftUI/ContentView.swift
+++ CheckResultwithUIKitInSwiftUI/ContentView.swift
@@ -8,9 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var targetValue = Int.random(in: 0...100)
+    @State private var currentValue = 1.0
+    @State private var isPresented = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 20) {
+            
+            HStack {
+                Text("Подвиньте слайдер, как можно ближе к:")
+                Text("\(targetValue)")
+                    .fontWeight(.bold)
+                    .frame(width: 40, height: 32, alignment: .trailing)
+            }
+            
+            HStack {
+                Text("0")
+                SliderUIKit(value: $currentValue, alpha: computeScore())
+                Text("100")
+            }
+            
+            Button("Проверь меня!", action: { isPresented.toggle() })
+                .alert("Ваш счет",
+                       isPresented: $isPresented,
+                       actions: {}) {
+                    Text("\(computeScore())")
+                }
+            
+            Button("Начать заново") {
+                targetValue = Int.random(in: 0...100)
+                currentValue = 0
+            }
+        }
+        .padding()
+    }
+
+    private func computeScore() -> Int {
+        let difference = abs(targetValue - lround(currentValue))
+        return 100 - difference
     }
 }
 
---
+++ CheckResultwithUIKitInSwiftUI/SliderUIKit.swift
@@ -0,0 +1,65 @@
+//
+//  SliderUIKit.swift
+//  CheckResultwithUIKitInSwiftUI
+//
+//  Created by Aleksandr Rybachev on 23.05.2022.
+//
+
+import SwiftUI
+
+struct SliderUIKit: UIViewRepresentable {
+
+    @Binding var value: Double
+    
+    let alpha: Int
+    
+    func makeUIView(context: Context) -> UISlider {
+        let slider = UISlider()
+        
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        
+        slider.tintColor = .blue
+        
+        slider.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.changedValue),
+            for: .valueChanged
+        )
+        
+        return slider
+    }
+    
+    func updateUIView(_ uiView: UISlider, context: Context) {
+        uiView.thumbTintColor = .red
+            .withAlphaComponent(CGFloat(alpha) / 100)
+        
+        uiView.value = Float(value)
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(value: $value)
+    }
+}
+
+extension SliderUIKit {
+    
+    class Coordinator: NSObject {
+        
+        @Binding var value: Double
+        
+        init(value: Binding<Double>) {
+            self._value = value
+        }
+        
+        @objc func changedValue(_ sender: UISlider) {
+            value = Double(sender.value)
+        }
+    }
+}
+
+struct SliderUI_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderUIKit(value: .constant(50), alpha: 8)
+    }
+}
